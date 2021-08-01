1b46dd456178f6115e45567823feb6acc36fd59c
ttran25678 <84260337+ttran25678@users.noreply.github.com>
2021-08-01 17:27:03 +0700

    buoi 2 - hipernate

---
+++ src/main/java/cybersoft/javabackend/java12/gira/role/entity/Role.java
@@ -0,0 +1,18 @@
+package cybersoft.javabackend.java12.gira.role.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import cybersoft.javabackend.java12.gira.common.entity.BaseEntity;
+
+@Entity
+@Table(name="gira_role")
+public class Role extends BaseEntity{
+
+	private String name;
+	private String description;
+	
+	@ManyToOne
+	private Group group;
+}
